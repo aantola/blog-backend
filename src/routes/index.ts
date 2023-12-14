fa1f8859ad262b193f5ed8553cb03fed6a53c06f
andres nahuel antola <aantola@dc.uba.ar>
2023-12-14 13:09:25 -0500

    feat(cors): Added cors using env variable

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,16 @@
 import express from 'express' 
 import UserRouter from 'Routes/user'
 import AuthRouter from 'Routes/auth'
+import cors from 'cors'
+
+let corsOptions = {
+    origin: process.env.FRONTEND_URL,
+    optionsSuccessStatus: 200,
+}
 
 let router = express.Router()
 
+router.use(cors(corsOptions))
 router.use('/auth', AuthRouter)
 router.use('/user', UserRouter)
 
